e71a611a38328a779847cc8de9d821b44c2f4963
KlausBrandner <k.brandner@5-designs.com>
2016-12-11 19:30:51 +0100

    project init

---
+++ app/src/main/java/com/a5_designs/recipefinder/HomeScreen.java
@@ -0,0 +1,14 @@
+package com.a5_designs.recipefinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+}
